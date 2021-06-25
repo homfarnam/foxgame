84bfb4da0e7f1538c417357b0aaaba86009b1cf7
homfarnam <homfarnam@gmail.com>
2021-06-26 00:04:34 +0430

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import 'tailwindcss/tailwind.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const catApi: string = process.env.CATS_API!;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+axios.defaults.headers.post['x-api-key'] = catApi;
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
