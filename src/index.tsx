024b56a83c6b9a43146ec63d5af6ac21f5703fc1
homfarnam <homfarnam@gmail.com>
2021-06-26 23:56:13 +0430

    Add toast container

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom';
-import 'tailwindcss/tailwind.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'tailwindcss/tailwind.css';
+import './styles/styles.css';
 
 const catApi: string = process.env.CATS_API!;
 
@@ -10,7 +13,19 @@ axios.defaults.headers.post['x-api-key'] = catApi;
 
 ReactDOM.render(
   <BrowserRouter>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
     <App />
+    <ToastContainer />
   </BrowserRouter>,
   document.getElementById('root')
 );
