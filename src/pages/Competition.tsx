0bc276f09a5c808b6217e9eba27210069e19a496
homfarnam <homfarnam@gmail.com>
2021-06-26 23:53:17 +0430

    Change box style

--- src/pages/Competition.tsx
+++ src/pages/Competition.tsx
@@ -1,14 +1,56 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import axios from 'axios';
+import moment from 'moment';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
+import { useHistory } from 'react-router';
+import { toast } from 'react-toastify';
 
 const Competition = () => {
   const [imagesData, setImagesData] = useState<
     { imgype: 'cat' | 'dog' | 'fox'; img: string }[]
   >([]);
-
   const [score, setScore] = useState<number>(0);
+  const [counter, setCounter] = useState<number>(30);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    let timer: any;
+    if (imagesData && counter > 0) {
+      timer = setInterval(() => setCounter((prev) => prev - 1), 1000);
+
+      if (counter === 0) {
+        toast.warning('Times up!!');
+      }
+    }
+
+    return () => clearInterval(timer);
+  }, [counter, imagesData]);
+
+  useEffect(() => {
+    const date = moment().format('MM/DD/YYYY');
+    if (counter === 0) {
+      const user = localStorage.getItem('user');
+      const users = JSON.parse(localStorage.getItem('users') || '[]');
+      const id = Math.random() * 1000;
+      let rank = 1;
+      localStorage.setItem('rank', rank.toString());
+      const getRank = localStorage.getItem('rank');
+      if (getRank) {
+        rank++;
+      }
+      users.push({
+        id: id,
+        user: user,
+        date: date,
+        score: score
+      });
+      localStorage.setItem('users', JSON.stringify(users));
+
+      history.push('/scores');
+    }
+  }, [counter, history, score]);
 
   const fetchAll = () => {
     const dog = axios.get('https://dog.ceo/api/breeds/image/random/4');
@@ -17,7 +59,6 @@ const Competition = () => {
 
     axios.all([dog, fox, cat]).then(
       axios.spread((...allData) => {
-        console.log(allData);
         const dogs = allData[0].data?.message?.map((item: any) => ({
           img: item,
           imgype: 'dog'
@@ -48,8 +89,11 @@ const Competition = () => {
   const showType = (item: { imgype: 'cat' | 'dog' | 'fox'; img: string }) => {
     console.log(item);
     if (item?.imgype === 'fox') {
-      setScore((prev) => prev + 1);
+      setScore((prev) => prev + 100);
       fetchAll();
+    } else {
+      toast.error('Wrong image');
+      setCounter((prev) => prev + 1);
     }
   };
 
@@ -57,19 +101,20 @@ const Competition = () => {
     <div className="w-full flex flex-col">
       <div className="w-full h-full flex flex-col items-center justify-center my-24">
         <h2 className="font-mono flex justify-center mb-10">Competition</h2>
-        <div className="w-full flex flex-row items-center justify-center ">
+        <div className="w-full flex flex-row items-center justify-center mb-10">
           <span className="mx-10">Score: {score}</span>
-          <span>Time left: </span>
+
+          <span>Time left: {counter}</span>
         </div>
-        <div className="border w-full h-auto">
-          <div className="flex flex-row justify-center flex-wrap">
-            <div>
+        <div className="w-1/2 h-auto">
+          <div className="bg-yellow-200 flex flex-row justify-center flex-wrap border">
+            <div className="p-5">
               {imagesData.slice(0, 3)?.map((item) => (
                 <img
                   src={item?.img}
                   width="200"
                   height="200"
-                  className="object-cover w-[200px]  h-[200px]"
+                  className="object-cover w-[200px] h-[200px] p-2 shadow-lg"
                   alt={item.img}
                   onClick={(e) => {
                     e.preventDefault();
@@ -78,13 +123,13 @@ const Competition = () => {
                 />
               ))}
             </div>
-            <div>
+            <div className="p-5">
               {imagesData.slice(3, 6)?.map((item) => (
                 <img
                   src={item?.img}
                   width="200"
                   height="200"
-                  className="object-cover w-[200px]  h-[200px]"
+                  className="object-cover w-[200px]  h-[200px] p-2 shadow-lg"
                   alt={item.img}
                   onClick={(e) => {
                     e.preventDefault();
@@ -93,13 +138,13 @@ const Competition = () => {
                 />
               ))}
             </div>
-            <div>
+            <div className="p-5">
               {imagesData.slice(6, 9)?.map((item) => (
                 <img
                   src={item?.img}
                   width="200"
                   height="200"
-                  className="object-cover w-[200px]  h-[200px]"
+                  className="object-cover w-[200px]  h-[200px] p-2 shadow-lg"
                   alt={item.img}
                   onClick={(e) => {
                     e.preventDefault();
