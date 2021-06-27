6503a3b7e08dd5df97b959d13e331b8d11fa17c2
homfarnam <homfarnam@gmail.com>
2021-06-27 12:20:24 +0430

    Create images component

---
+++ src/components/Images/Images.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+interface ImageProps {
+  imagesData: { imgype: 'cat' | 'dog' | 'fox'; img: string }[];
+  showType: (item: { imgype: 'cat' | 'dog' | 'fox'; img: string }) => void;
+}
+
+const Images: React.FC<ImageProps> = ({ imagesData, showType }) => {
+  return (
+    <div className="bg-yellow-200 flex flex-row justify-center flex-wrap border">
+      <div className="p-5">
+        {imagesData.slice(0, 3)?.map((item) => (
+          <img
+            src={item?.img}
+            width="200"
+            height="200"
+            className="object-cover w-[200px] h-[200px] p-2 shadow-lg"
+            alt={item.img}
+            onClick={(e) => {
+              e.preventDefault();
+              showType(item);
+            }}
+          />
+        ))}
+      </div>
+      <div className="p-5">
+        {imagesData.slice(3, 6)?.map((item) => (
+          <img
+            src={item?.img}
+            width="200"
+            height="200"
+            className="object-cover w-[200px]  h-[200px] p-2 shadow-lg"
+            alt={item.img}
+            onClick={(e) => {
+              e.preventDefault();
+              showType(item);
+            }}
+          />
+        ))}
+      </div>
+      <div className="p-5">
+        {imagesData.slice(6, 9)?.map((item) => (
+          <img
+            src={item?.img}
+            width="200"
+            height="200"
+            className="object-cover w-[200px]  h-[200px] p-2 shadow-lg"
+            alt={item.img}
+            onClick={(e) => {
+              e.preventDefault();
+              showType(item);
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Images;
